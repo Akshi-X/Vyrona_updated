d84309fda521e064ffa5b57ace687a039bcd4f61
nishaanth-mg <nishaanth@mygrape.org>
2026-04-02 11:08:06 +0530

    fix: update icon alt texts and sources for Quality Deviations and Deviation Driver in Dashboard component

--- FrontEnd/src/pages/Dashboard/index.tsx
+++ FrontEnd/src/pages/Dashboard/index.tsx
@@ -1205,7 +1205,7 @@ export default function Dashboard({ }: DashboardProps) {
                       <div className="flex flex-col bg-white border border-[#E7E1E1] rounded-lg p-3 h-[123px]">
                         <div className="flex flex-col items-start mb-2 ml-3">
                           <div className="w-8 h-8 bg-[#fdf1ff] rounded-2xl flex items-center justify-center">
-                            <img className="w-[18px] h-[18px]" alt="Outbound Shipment" src={OutboundShipmentIcon} />
+                            <img className="w-[18px] h-[18px]" alt="Quality Deviations" src={CriticalAlertsIcon} />
                           </div>
                           <div className="font-normal text-[#656565] text-[11px] mt-2">
                             Quality Deviations Flagged
@@ -1225,7 +1225,7 @@ export default function Dashboard({ }: DashboardProps) {
                       <div className="flex flex-col bg-white border border-[#E7E1E1] rounded-lg p-3 h-[123px]">
                         <div className="flex flex-col items-start mb-2 ml-3">
                           <div className="w-8 h-8 bg-[#fdf1ff] rounded-2xl flex items-center justify-center">
-                            <img className="w-[18px] h-[18px]" alt="Avg Quality Lost Patient" src={AvgQualityLostPatientIcon} />
+                            <img className="w-[18px] h-[18px]" alt="Deviation Driver" src={DeviationDriverIcon} />
                           </div>
                           <div className="font-normal text-[#656565] text-[11px] mt-2">
                           Top Deviation Driver
