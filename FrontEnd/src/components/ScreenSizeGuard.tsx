361e91c5f43146e731ec5110d78943f9df3202cc
sreejithns-mygrape <sreejith@mygrape.org>
2026-04-02 11:00:39 +0530

    refactor: separate type import for ReactNode in ScreenSizeGuard component

--- FrontEnd/src/components/ScreenSizeGuard.tsx
+++ FrontEnd/src/components/ScreenSizeGuard.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import type {ReactNode} from "react";
 
 type ScreenSizeGuardProps = {
     children: ReactNode;
