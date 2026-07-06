1e88e9954a160bbbd934d8a2d2a3ebd00e9a7e0c
sreejithns-mygrape <sreejith@mygrape.org>
2026-07-06 17:49:30 +0000

    fix: remove commented-out code in UserProfilePage component

--- FrontEnd/src/pages/UserProfilePage/index.tsx
+++ FrontEnd/src/pages/UserProfilePage/index.tsx
@@ -686,7 +686,7 @@ const UserProfilePage: React.FC = () => {
               </svg>
             </button>
           </div>
-        )} */}
+        )}
 
         {/* Support Activity Section */}
         <div id="onboarding-profile-support-activity" className="bg-white rounded-xl shadow-sm border border-gray-100 p-8">
